fac09c9d81ae3ba1d0e9b62d6c39e44843219e42
shen <2030408929@qq.com>
2025-11-25 15:35:58 +0800

    11/25

--- sandbox/CuDNN_test.py
+++
@@ -1,4 +0,0 @@
-import torch
-print("CuDNN available:", torch.backends.cudnn.enabled)
-print(torch.cuda.is_available())  # 检查是否有可用的 CUDA 设备
-print(torch.version.cuda)          # 检查 PyTorch 支持的 CUDA 版本
